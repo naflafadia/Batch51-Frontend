4b934717243406e91306591e699bf0fad1217b60
Nafla Fadia <naflafadia0@gmail.com>
2023-12-23 22:25:31 +0700

    new feature updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ProvinceSelector from './components/ProvinceSelector';
 import RegencySelector from './components/RegencySelector';
 import DistrictSelector from './components/DistrictSelector';
@@ -7,16 +7,38 @@ import './styles/main.css';
 const App: React.FC = () => {
   const [selectedProvince, setSelectedProvince] = useState('');
   const [selectedRegency, setSelectedRegency] = useState('');
+  const [shouldRefresh, setShouldRefresh] = useState(false);
+
+  useEffect(() => {
+    if (shouldRefresh) {
+      window.location.reload();
+      setShouldRefresh(false);
+    }
+  }, [shouldRefresh]);
+
+  const handleProvinceSelect = (provinceId: string) => {
+    if (selectedProvince && selectedRegency) {
+      setShouldRefresh(true);
+    }
+    setSelectedProvince(provinceId);
+    setSelectedRegency('');
+  };
+
+  const handleRegencySelect = (regencyId: string) => {
+    setSelectedRegency(regencyId);
+  };
 
   return (
     <div>
       <h1>Data Wilayah Indonesia</h1>
-      <ProvinceSelector onSelect={setSelectedProvince} />
+      <ProvinceSelector onSelect={handleProvinceSelect} />
       {selectedProvince && (
-        <RegencySelector provinceId={selectedProvince} onSelect={setSelectedRegency} />
-      )}
-      {selectedRegency && (
-        <DistrictSelector regencyId={selectedRegency} onSelect={() => {}} />
+        <>
+          <RegencySelector provinceId={selectedProvince} onSelect={handleRegencySelect} />
+          {selectedRegency && (
+            <DistrictSelector regencyId={selectedRegency} onSelect={() => {}} />
+          )}
+        </>
       )}
     </div>
   );
